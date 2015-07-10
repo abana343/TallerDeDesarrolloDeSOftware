16446ee50e1a3ca9c34047bdb42473b7aabbd423
abana343 <victor_ayala343@hotmail.com>
2015-07-10 05:35:38 -0300

    Mysurfacethread

--- app/src/main/java/com/example/bastian/nuevo2/MySurfaceThread.java
+++ app/src/main/java/com/example/bastian/nuevo2/MySurfaceThread.java
@@ -9,40 +9,40 @@ import android.view.SurfaceView;
  */
 public class MySurfaceThread extends Thread
 {
-    private SurfaceHolder sh;
-    private SurfaceView view;
-    private boolean run;
+    private SurfaceHolder _sh;
+    private SurfaceView _view;
+    private boolean _run;
 
     //el constructor recive los dos tipos de surface
     public MySurfaceThread(SurfaceHolder sh, SurfaceView view)
     {
-        this.sh=sh;
-        this.view=view;
-        this.run=false;
+        this._sh = sh;
+        this._view =view;
+        this._run =false;
     }
 
     //se utiliza para establecer si esta corriendo o no
     public void setRuning(boolean run)
     {
-        this.run=run;
+        this._run =run;
     }
 
     public void run()
     {
         //se instancia el canvas
         Canvas canvas;
-        while(run)
+        while(_run)
         {
             canvas = null;
             try
             {
                 //se define nulo el area de pintado
-                canvas = sh.lockCanvas(null);
+                canvas = _sh.lockCanvas(null);
                 //se utiliza el Synchronized  para asegurarce que no se este usando el objeto en ningun otro thread
-                synchronized (sh)
+                synchronized (_sh)
                 {
                     //se ejecuta el metodo para que dibuje pasandole en canvas
-                    this.view.draw(canvas);
+                    this._view.draw(canvas);
                 }
             }
             finally
@@ -50,7 +50,7 @@ public class MySurfaceThread extends Thread
                 if(canvas != null)
                 {
                     //se libera el canvas
-                    sh.unlockCanvasAndPost(canvas);
+                    _sh.unlockCanvasAndPost(canvas);
                 }
             }
         }
